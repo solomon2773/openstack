e977878a87e60168ab966c329eae4136127ba4c1
solomon2773 <solomon2773@yahoo.com>
2019-01-21 18:45:11 -0600

    Update openstack_allinone_install.sh

--- openstack_allinone_install.sh
+++ openstack_allinone_install.sh
@@ -4,20 +4,15 @@ START=$(date +%s.%N)
 echo "Network Setup"
 
 systemctl disable firewalld
-
 systemctl stop firewalld
-
 systemctl disable NetworkManager
-
 systemctl stop NetworkManager
-
 systemctl enable network
-
 systemctl start network
 
 ###   Software repositories
 echo "Software Repositories Setup"
-yum install -y https://rdoproject.org/repos/rdo-release.rpm
+
 yum install -y centos-release-openstack-rocky
 yum install -y yum-utils --enablerepo="base"
 yum-config-manager --enable openstack-rocky
@@ -26,14 +21,15 @@ yum update -y
 
 #### Install Packstack Installer
 ##echo "Install Packstack Installer"
-yum install -y openstack-packstack
+
+yum install -y openstack-packstack 
 
 
 #### Run Packstack to install OpenStack
 
 echo "Run Packstack to install OpenStack All in One"
-
-packstack --allinone
+packstack --gen-answer-file=answers.txt
+packstack --allinone --answer-file=answers.txt
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
